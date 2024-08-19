3113d6f218708fd58c8a92895c8d684c4e507c7d
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-20 08:56:27 +0900

    Created User Bean and UserDaoService

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,24 @@
+package com.in28minutes.rest.webservices.restful_web_services.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	
+	private static List<User> users = new ArrayList<>();
+	
+	static {
+		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+		users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+		users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+	}
+	
+	public List<User> findAll() {
+		return users;
+	}
+
+}
