d7eb7c769bb441fa441236cc2253cbca864e8dbc
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-20 20:42:54 +0900

    Enhanced the Post method to return correct HTTP status code and location

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,12 +1,15 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -28,8 +31,15 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public void createrUser(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<User> createrUser(@RequestBody User user) {
+		
+		User savedUser = service.save(user);		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		
+		return ResponseEntity.created(location).build();
 		
 	}
 
