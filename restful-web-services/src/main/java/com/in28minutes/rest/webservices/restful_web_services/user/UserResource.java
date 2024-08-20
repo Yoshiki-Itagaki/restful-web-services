3b5aa83880e69861eb667816c5788c21ea764a55
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-20 21:15:20 +0900

    Implemented exception handling - 404 resource not found

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -27,7 +27,14 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User retriveUser(@PathVariable int id){
-		return service.findOne(id);		
+		
+		User user = service.findOne(id);
+		
+		if(user==null) {
+			throw new UserNotFoundException("id: " + id);
+		}
+		
+		return user;		
 	}
 	
 	@PostMapping("/users")
