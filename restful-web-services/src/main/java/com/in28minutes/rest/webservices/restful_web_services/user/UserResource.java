610c21c93adb0d4d5092dba3fc9d448ae0557fa2
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-20 20:26:43 +0900

    Implemented a POST method to create a user

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +25,12 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retriveUser(@PathVariable int id){
 		return service.findOne(id);		
+	}
+	
+	@PostMapping("/users")
+	public void createrUser(@RequestBody User user) {
+		service.save(user);
+		
 	}
 
 }
