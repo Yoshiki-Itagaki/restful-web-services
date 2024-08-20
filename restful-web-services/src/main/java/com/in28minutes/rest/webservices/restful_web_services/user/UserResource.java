357e61878c2565ae632a8979704d71ec4e5e544f
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-20 21:57:09 +0900

    Implemented DELETE method to delete a user resource

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,11 @@ public class UserResource {
 		}
 		
 		return user;		
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id){
+		service.deleteById(id);
 	}
 	
 	@PostMapping("/users")
