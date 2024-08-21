76925060d7736b5afe001ca65844d1d33b222d8f
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-21 09:25:09 +0900

    Versioning REST API - URI Versioning

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/versioning/PersonV1.java
@@ -0,0 +1,21 @@
+package com.in28minutes.rest.webservices.restful_web_services.versioning;
+
+public class PersonV1 {
+	
+	private String name;
+
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonV1 [name=" + name + "]";
+	}
+
+}
