d25b4d72384a14e9ca2f8f5252601f4de1a0ef0f
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-21 19:00:41 +0900

    Implemented Static Filtering for REST API

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -17,9 +19,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min=2, message = "Name should have at least 2 characters.")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "Birth Date should be in the past.")
+	@JsonProperty("birth_date")
 	private LocalDate birthdate;
 
 	public Integer getId() {
